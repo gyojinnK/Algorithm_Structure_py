1a53529d288e1fbe4b3d0ff9408454cca7cc4966
gyojinnK <ksmug1998@gmail.com>
2024-01-03 15:06:58 +0900

    큐 문제

---
+++ bj_2164.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+
+def solution(N):
+    deq = deque([i for i in range(1, N+1)])
+    while len(deq) > 1:
+        deq.popleft()
+        restore = deq.popleft()
+        deq.append(restore)
+
+    print(deq.popleft())
+
+solution(6)
