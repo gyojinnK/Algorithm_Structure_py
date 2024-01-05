7b8628f14602e8c6c1a8bb0e413574d55a46c2d1
gyojinnK <ksmug1998@gmail.com>
2024-01-05 14:17:41 +0900

    해시맵과 힙

---
+++ TopKFrequentElements.py
@@ -0,0 +1,22 @@
+import collections
+import heapq
+from typing import List
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = collections.Counter(nums)
+        res = []
+        print(counter)
+
+        h = []
+        for num in counter:
+            heapq.heappush(h, [-counter[num], num])
+
+        for _ in range(k):
+            poped = heapq.heappop(h)
+            res.append(poped[1])
+
+        return sorted(res)
+
+s = Solution()
+print(s.topKFrequent([1,2,2,3,3,3], 2))
