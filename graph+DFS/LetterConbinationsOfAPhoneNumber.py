e1c915cc797cdc0c0a9138f56e293c7c6cb3ce16
gyojinnK <ksmug1998@gmail.com>
2024-01-12 08:44:28 +0900

    카디션 프로덕트

--- graph+DFS/LetterConbinationsOfAPhoneNumber.py
+++ graph+DFS/LetterConbinationsOfAPhoneNumber.py
@@ -4,28 +4,33 @@
 전화 버튼과 마찬가지로 숫자를 문자로 매핑하는 방법은 다음과 같습니다.
 1은 어떤 문자에도 매핑되지 않습니다.
 '''
+# 참고: itertools
+# https://soundprovider.tistory.com/entry/python-itertools
+from itertools import product
 from typing import List
-
-code = {
-    '2':['a','b','c'],
-    '3':['d','e','f'],
-    '4':['g','h','i'],
-    '5':['j','k','l'],
-    '6':['m','n','o'],
-    '7':['p','q','r','s'],
-    '8':['t','u','v'],
-    '9':['w','x','y','z']
-}
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        pass
+        if not digits:
+            return []
+        # len = 9
+        digitsToken = [
+            [], ['abc'], ['def'], ['ghi'], ['jkl'], ['mno'], ['pqrs'], ['tuv'], ['wxyz']
+        ]
+        arr = []
+        res = []
+        for t in digits:
+            arr += digitsToken[int(t)-1]
 
+        combi = product(*arr)
+        for el in combi:
+            temp = ''
+            for t in el:
+                temp += t
+            res.append(temp)
 
-
-
-
+        return res
 
 
 s = Solution()
-s.letterCombinations("234")
+s.letterCombinations("23")
