14d40c868fb4eef08bbe6b4e283153d3c7b6134c
gyojinnK <ksmug1998@gmail.com>
2024-01-12 22:46:59 +0900

    균형 이진 트리

---
+++ tree+binaryTree/BalancedBinaryTree.py
@@ -0,0 +1,26 @@
+'''
+이진 트리가 주어지면 높이 균형이 맞는지 확인합니다.
+높이 균형: 모든 노드의 서브 트리 간의 높이 차이가 1이하인 것
+'''
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def check(root):
+            if not root:
+                return 0
+
+            left = check(root.left)
+            right = check(root.right)
+
+            if left == -1 or right == -1 or abs(left - right) > 1:
+                return -1
+            return max(left, right) + 1
+
+        return check(root) != -1
